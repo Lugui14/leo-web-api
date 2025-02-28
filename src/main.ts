2b4126dde594c2abca3de549aa7f7e439bc74f41
luiz <luiz.lopes@desbravador.com.br>
2025-02-28 17:55:56 -0300

    fix: swagger description

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,9 @@ async function bootstrap() {
     });
 
     const swaggerConfig = new DocumentBuilder()
-        .setTitle("Cats example")
-        .setDescription("The cats API description")
+        .setTitle("Leo Web API")
+        .setDescription("API for Leo Club")
         .setVersion("1.0")
-        .addTag("cats")
         .build();
 
     const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
